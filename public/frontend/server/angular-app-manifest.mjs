54f2ad2ff5b195a5288a05058695bc9b9ec7f557
IdeaQru <fiqru.s@gmail.com>
2025-10-30 19:37:15 +0700

    301025-1937

--- public/frontend/server/angular-app-manifest.mjs
+++ public/frontend/server/angular-app-manifest.mjs
@@ -33,7 +33,7 @@ export default {
     "chunk-X4FUVRDZ.js"
   ],
   "src/app/pages/registration/registration.ts": [
-    "chunk-IHEYDTVE.js",
+    "chunk-ZWRKJZD3.js",
     "chunk-7MYJHMX5.js",
     "chunk-IE5OCRMK.js",
     "chunk-X4FUVRDZ.js"
@@ -63,8 +63,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 4408, hash: '54ffb61b7a27c7d365247edd13756f650715ccd18724c1555cf113688d914699', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 4071, hash: '3d038b250db5582cffc8478bbb4408bf1c3387121dcbfa3fb9c07babac10273b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 4408, hash: '12abc45998922b48d8f842972c31c465331aa2d656fb9b73dd61a790246162ec', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 4071, hash: '54e7a8510ddd1d4c6635a34cba597961c79590370ae1c7485716a276869f4594', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-Q63L6MFV.css': {size: 2052, hash: 'lKpZgfq6xJc', text: () => import('./assets-chunks/styles-Q63L6MFV_css.mjs').then(m => m.default)}
   },
 };
