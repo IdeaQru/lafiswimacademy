10330e6142822605eb6a13353f4d94067ac4c313
IdeaQru <fiqru.s@gmail.com>
2025-11-04 08:35:38 +0700

    news0311835

--- public/frontend/server/angular-app-manifest.mjs
+++ public/frontend/server/angular-app-manifest.mjs
@@ -13,14 +13,14 @@ export default {
     "chunk-H6LQATRN.js"
   ],
   "src/app/pages/sidebar/schedule/schedule.ts": [
-    "chunk-NNBMAKTG.js",
+    "chunk-7FIWOSNQ.js",
     "chunk-R52DZ4SA.js",
     "chunk-S2YFGWBM.js",
     "chunk-KAYWDR7T.js",
     "chunk-H6LQATRN.js"
   ],
   "src/app/pages/sidebar/reports/reports.ts": [
-    "chunk-NABNT2ES.js",
+    "chunk-VEP2AHC5.js",
     "chunk-H6LQATRN.js"
   ],
   "src/app/pages/home/home.ts": [
@@ -29,7 +29,7 @@ export default {
     "chunk-KAYWDR7T.js"
   ],
   "src/app/pages/login/login.ts": [
-    "chunk-NL4RV4DI.js",
+    "chunk-GS4JRQUA.js",
     "chunk-H6LQATRN.js"
   ],
   "src/app/pages/registration/registration.ts": [
@@ -39,11 +39,11 @@ export default {
     "chunk-H6LQATRN.js"
   ],
   "src/app/pages/dashboard/dashboard.ts": [
-    "chunk-3YRN2KJ5.js",
+    "chunk-6LPUHV43.js",
     "chunk-S2YFGWBM.js"
   ],
   "src/app/pages/sidebar/dashboard-home/dashboard-home.ts": [
-    "chunk-OY6GGI43.js"
+    "chunk-IY7TATLY.js"
   ],
   "src/app/pages/sidebar/user-configuration/user-configuration.ts": [
     "chunk-JYM4ZF7U.js",
@@ -63,8 +63,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 8921, hash: 'b19a4cc61c730433830008a20f5fad6a6ab5f88941e7539c71669190b20f0ef2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 8584, hash: '8f36987cacbcb3228b5ee16fd511b00a0dcef6074ec85d16a59108426c4416f2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 8921, hash: '7e32f2b7f08808d7693fbbfac8fbdcd653262196be4d28818e19a2e15a3553f9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 8584, hash: 'd3e7babfe51e1b9332e8add0090ef703dcad213f1aeb68c93837caa9b9090984', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-Q63L6MFV.css': {size: 2052, hash: 'lKpZgfq6xJc', text: () => import('./assets-chunks/styles-Q63L6MFV_css.mjs').then(m => m.default)}
   },
 };
