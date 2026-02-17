8a02cd913ac1cd4aee82c9793ceee4f3b9e8d6b7
IdeaQru <fiqru.s@gmail.com>
2026-02-17 18:49:10 +0700

    17021849

--- public/frontend/server/angular-app-manifest.mjs
+++ public/frontend/server/angular-app-manifest.mjs
@@ -13,7 +13,7 @@ export default {
     "chunk-ZEOML7JY.js"
   ],
   "src/app/pages/sidebar/schedule/schedule.ts": [
-    "chunk-WZOWNSDI.js",
+    "chunk-RLAKQMDL.js",
     "chunk-YZIDNEVJ.js",
     "chunk-WSTRFGGB.js",
     "chunk-BM7JHERH.js",
@@ -67,8 +67,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 32892, hash: '090d663f3ac5f751ebc467a3356ef3cc846d808f43c45ca21b0cc4b8fa4f1029', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 24621, hash: 'c12910a6de779bb31c5b4400bdbe512feab9efca5bdee41d335cb1f3e0b45a25', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 32892, hash: '095c7a1da3387fbbc2db2a008a2d1e1befced34836e153ae806d843e091b29de', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 24621, hash: '70acffcd8422e34abee1f71632dd1512344d81a7b41440b5eb9f0802445fc968', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5IICDY77.css': {size: 10151, hash: 'bgIYC/G4a7E', text: () => import('./assets-chunks/styles-5IICDY77_css.mjs').then(m => m.default)}
   },
 };
